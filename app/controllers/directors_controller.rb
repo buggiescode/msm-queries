368cea2b67a2ec160d97b6f844a04387e56cc3ea
buggiescode <137247769+buggiescode@users.noreply.github.com>
2025-05-13 22:45:00 +0000

    Add directors view

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,15 @@
+class DirectorsController < ApplicationController
+  def index
+    render({ :template => "director_templates/list"})
+  end
+  def show
+    director_id = params.fetch("director_id")
+
+    matching_directors = Director.where({:id => director_id})
+    @director = matching_directors.first()
+
+    @matching_movies = Movie.where({ :director_id => @director.id })
+    
+    render({ :template => "director_templates/details"})
+  end
+end
